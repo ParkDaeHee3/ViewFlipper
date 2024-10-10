a154f2f33626b872563a7d7912cf6779cc244aa5
ParkDaeHee3 <156628364+ParkDaeHee3@users.noreply.github.com>
2024-10-10 17:55:48 +0900

    imageView_version
    imageView_version_update

--- app/src/main/java/com/example/viewflipper/MainActivity.java
+++ app/src/main/java/com/example/viewflipper/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.viewflipper;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,14 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnPrev, btnNext;
+        //Button btnPrev, btnNext; // linearlayout version
+        Button btnStart, btnStop;
         final ViewFlipper vFlipper;
 
-        btnPrev = (Button) findViewById(R.id.btnPrev);
-        btnNext = (Button) findViewById(R.id.btnNext);
+        //btnPrev = (Button) findViewById(R.id.btnPrev); //linearlayout version
+        //btnNext = (Button) findViewById(R.id.btnNext); // linearlayout version
 
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStop = (Button) findViewById(R.id.btnStop);
         vFlipper=(ViewFlipper) findViewById(R.id.viewFlipper1);
 
+        /* [linearlayout_version]
         btnPrev.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,5 +38,19 @@ public class MainActivity extends AppCompatActivity {
                 vFlipper.showNext();
             }
         });
+        */
+        vFlipper.setFlipInterval(1000);
+        btnStart.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                vFlipper.startFlipping();
+                vFlipper.setFlipInterval(1000);
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                vFlipper.stopFlipping();
+            }
+        });
+
     }
 }
